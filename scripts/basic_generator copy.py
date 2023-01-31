05f1000d3e81d1e28a8accca08ecf1b8a363dfd3
Heesom <heesom.hs@gmail.com>
2023-01-31 16:36:05 +0900

    test[scripts]: 기본 리드미 자동 배포 코드 작성

--- scripts/basic_generator copy.py
+++ scripts/basic_generator copy.py
@@ -1,14 +1,23 @@
 from typing import *
-import urllib
+from urllib import parse
 import os
-from datetime import datetime
-
-def get_list_of_problem_folders_raw():
-    return [f for f in os.listdir('00_발표')]
-
 
 def get_list_of_presentation_folders_raw():
-    return [f for f in os.listdir('01_문제')]
+    my_url = os.path.abspath('.\\00_발표')
+
+    folders = [f for f in os.listdir(my_url)]
+    folders.remove('README.md')
+    
+    return folders
+
+
+
+def get_list_of_problem_folders_raw():
+    my_url = os.path.abspath('.\\01_문제')
+    
+    folders = [f for f in os.listdir(my_url)]
+
+    return folders
 
 def folder_format(raw_folder):
     result = ''
@@ -18,52 +27,50 @@ def folder_format(raw_folder):
             c = int(c)
         except:
             result += c
-    result.lstrip('_')
-    result.replace('_',", ")
+    result = result.strip('_')
+    result = result.replace('_',", ")
 
     return result
 
 
 def parse_directory_path(folder):
-    my_url = urllib.parse.quote(folder)
+    
+    my_url = parse.quote(folder)
     return f'https://github.com/Maker-H/GroupStudy_Algo_Log/tree/master/{my_url}'
 
 
-def date_format():
-    my_year = datetime.today().year
-    my_month = datetime.today().month
-    my_day = datetime.today().day
+def date_format(folder):
+    date = int("".join(folder[-6:]))
+    my_year = date // 10000
+    my_month = (date % 10000) // 100
+    my_date = date % 100
+    date = f'20{my_year}-{my_month}-{my_date}'
 
-    return f'{my_year}-{my_month}-{my_day}'
+    return date
 
 
 def get_column_number(f):
     num = 0
+    rows = []
+    row = []
+    while True:
+        tmp = f.readline()
+        row = list(tmp)
+        rows.append(row)
+        if not tmp:
+            break
+    
 
-    flag = True
-    while flag:
-        one_line = list(f.readline())
-        print(one_line)
-        if one_line[-1] == '|':
-            tmp = 0
-            for c in list(f.readline()):
-                if c == ' ':
-                    tmp += 1
-                if tmp == 10:
-                    break
-
-        cnt = 0
-        
-        for idx in range(len(one_line)):
-            if one_line[idx] == '|':
-                cnt += 1
-            if cnt == 3:
-                num = one_line[idx-1: idx]
-                my_str = "".join(num)
-                print(num)
-                num = int(my_str)
-                flag = False
-                break
+    that_row = rows[-1]
+    cnt = 0
+    for idx in range(len(that_row)):
+        if that_row[idx] == '|':
+            cnt += 1
+        if cnt == 3:
+            num = that_row[idx-1: idx]
+            my_str = "".join(num)
+            num = int(my_str)
+            break
     
     if num < 9:
         return f'0{num}'
@@ -74,24 +81,42 @@ def get_column_number(f):
 
 # 날짜, 순번, 주제(url), 문제(url)
 def make_table_row(raw_presentation_new_folder, raw_problem_new_folder, column_number):
-    return f'|{date_format}|{column_number}|[{folder_format(raw_presentation_new_folder)}]({parse_directory_path(raw_presentation_new_folder)})|[바로가기]({parse_directory_path(raw_problem_new_folder)})|'
+    return f'|{date_format(raw_presentation_new_folder)}|{column_number}|[{folder_format(raw_presentation_new_folder)}]({parse_directory_path(raw_presentation_new_folder)})|[바로가기]({parse_directory_path(raw_problem_new_folder)})|'
     
 
 
 
 if __name__ == "__main__":
-    # readme.md 파일을 생성한다.
+    # 갱신되는 테이블을 만든다.
     f = open('scripts/readme_table.md', "r", encoding='utf-8')
+    # TODO: 순번 잘 받아오기 잘 출력안됨...
     column_number = get_column_number(f)
-    table_row = make_table_row(get_list_of_presentation_folders_raw()[-1], get_list_of_problem_folders_raw()[-1],column_number)
-    f = open('scripts/readme_table.md', "w", encoding='utf-8')
-    f.writelines(table_row)
 
-    f = open('README1.md', "w", encoding='utf-8')
+    # TODO: 만약 더 추가할 필요없이 잘 들어 있으면 굳이 추가해서 갱신할 필요 없음
+    # TODO: 제목은 스택, 큐, 덱 - 발표자 의 형식이여야 함
+    table_row = make_table_row(get_list_of_presentation_folders_raw()[-1], get_list_of_problem_folders_raw()[-1],column_number)
+    f = open('scripts/readme_table.md', "a", encoding='utf-8')
+    
+    print(table_row)
     f.writelines(table_row)
-    f.writelines()
-    n = open('scripts/template.md', "w", encoding='utf-8')
-    f.writelines(n.read())
+    f.close()
+
+    # 리드미 작성
+    f = open('README1.md', "w", encoding='utf-8')
+    n = open('scripts/readme_table.md', "r", encoding='utf-8')
+    t = open('scripts/template.md', "r", encoding='utf-8')
+    
+    while True:
+        new_n = n.readline()
+        f.writelines(new_n)
+        if not new_n:
+            break
+
+    while True:
+        t_line = t.readline()
+        f.writelines(t_line)
+        if not t_line:
+            break
     
     n.close()
     f.close()
