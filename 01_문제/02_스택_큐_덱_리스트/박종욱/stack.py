cd77c7825b10fbe1a46d40d28c4d007b27addc20
박종욱 <pju7429@naver.com>
2023-02-04 17:22:11 +0900

    파일이동

--- 01_문제/02_스택_큐_덱_리스트/박종욱/stack.py
+++ 01_문제/02_스택_큐_덱_리스트/박종욱/stack.py
@@ -1,33 +1,3 @@
-# class Stack:
-
-#     def __init__(self):
-#         self.name = []
-
-#     def push(self,num):
-#         self.name.append(num)
-
-#     def pop(self):
-#         if len(self.name) >= 1:
-#             self.name.remove(self.name[-1])
-#             return self.name.remove(self.name[-1])
-#         else:
-#             return -1
-    
-#     def size(self):
-#         return len(self.name)
-    
-#     def empty(self):
-#         if len(self.name) == 0:
-#             return 1
-#         else:
-#             return 0
-
-#     def top(self):
-#         if len(self.name) == 0:
-#             return -1
-
-#         else:
-#             return self.name[-1]
 
 import sys
 list = []
