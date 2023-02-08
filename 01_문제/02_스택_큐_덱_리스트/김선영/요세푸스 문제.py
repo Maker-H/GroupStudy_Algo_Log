ce16393166bdfed6cde995c38dc6de835656e596
ksy <rzlzlq@naver.com>
2023-02-09 03:22:33 +0900

    [김선영] 스택/요세푸스 문제/실버

--- 01_문제/02_스택_큐_덱_리스트/김선영/요세푸스 문제.py
+++ 01_문제/02_스택_큐_덱_리스트/김선영/요세푸스 문제.py
@@ -10,7 +10,7 @@ for person in range(1, n+1):
 
 ans = []
 while q:
-    for _ in range(k-1):         # K-1번째까지 빼서 뒤에 넣음
+    for _ in range(k-1):         # K-1번째까지 앞에서 빼서 뒤에 넣음
         q.append(q.popleft())
     ans.append(str(q.popleft()))            # K번째는 없앰
 ans_format = ', '.join(ans)
