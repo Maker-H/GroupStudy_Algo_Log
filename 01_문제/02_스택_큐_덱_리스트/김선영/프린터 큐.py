b7149f9e2823e901c25a592db781d801b30b6790
ksy <rzlzlq@naver.com>
2023-02-09 03:34:09 +0900

    [김선영] 스택/프린터 큐/실버

---
+++ 01_문제/02_스택_큐_덱_리스트/김선영/프린터 큐.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for tc in range(1, T+1):
+    n, m = map(int, input().split())
+    q = deque(map(int, input().split()))
+
+    target = deque()      # target의 idx를 추적하기 위한 덱
+    for _ in range(n):
+        target.append(0)
+    target[m] = 'target'    # target 지정
+
+    result = []
+    while q:
+        if q[0] != max(q):                      # 첫 문서가 제일 중요한 문서가 아닐 때
+            q.append(q.popleft())
+            target.append(target.popleft())
+        else:                                   # 첫 문서가 제일 중요한 문서일 때
+            q.popleft()
+            result.append(target.popleft())     # 제거된 문서 result에 담기
+
+    print(result.index('target')+1)             # target의 index + 1 출력
