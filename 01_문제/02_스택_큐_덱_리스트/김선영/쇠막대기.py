722c4810857b1c63096aacd600f989f0acebf338
ksy <rzlzlq@naver.com>
2023-02-09 03:42:05 +0900

    [김선영] 스택/쇠막대기/실버

--- 01_문제/02_스택_큐_덱_리스트/김선영/쇠막대기.py
+++ 01_문제/02_스택_큐_덱_리스트/김선영/쇠막대기.py
@@ -5,13 +5,13 @@ par = input().rstrip()
 stack = []
 count = 0
 for idx in range(len(par)):
-    if par[idx] == '(':
+    if par[idx] == '(':         # '('이 나오면 stack에 push
         stack.append(par[idx])
-    else:
-        if par[idx-1] =='(':
-            stack.pop()
-            count += len(stack)
-        else:
-            stack.pop()
-            count += 1
+    else:                       # ')'이 나오면
+        if par[idx-1] =='(':    # 바로 앞이 '('이면 레이저이므로
+            stack.pop()         # stack에서 레이저를 구성하는 '('를 하나 지우고
+            count += len(stack) # stack에 들어있던 '('의 수를 count에 누적
+        else:                   # 레이저가 아니면 쇠막대기이므로
+            stack.pop()         # stack에서 쇠막대기를 구성하는 '('를 하나 지우고
+            count += 1          # 그 막대기의 끝부분의 조각을 하나 count
 print(count)
