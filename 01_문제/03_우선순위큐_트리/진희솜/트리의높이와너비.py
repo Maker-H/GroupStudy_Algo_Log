c8c105edd0f698856674dadf05438df89957f1a4
Heesom <heesom.hs@gmail.com>
2023-02-23 12:23:30 +0900

    [진희솜] 트리 / 트리의 높이와 너비 / 골드
    https://www.acmicpc.net/problem/2250

--- 01_문제/03_우선순위큐_트리/진희솜/트리의높이와너비.py
+++ 01_문제/03_우선순위큐_트리/진희솜/트리의높이와너비.py
@@ -6,7 +6,7 @@ def inorder(current):
         inorder(tree[current][0])
 
     cur += 1
-    
+
     if level_min[level[current]] > cur:
         level_min[level[current]] = cur
     if level_max[level[current]] < cur:
@@ -20,34 +20,32 @@ def inorder(current):
     return
 
 
-
-N = int(input())  
-parentNode = [-1] * (N+1)
-tree = [[0] for _ in range(N+1)]
+N = int(input())
+parentNode = [-1] * (N + 1)
+tree = [[0] for _ in range(N + 1)]
 root = 0
 
 for _ in range(N):
     v, left, right = map(int, input().split())
 
     tree[v] = [left, right]
-    
+
     if left != -1:
         parentNode[left] = v
-    
+
     if right != -1:
         parentNode[right] = v
 
 # 루트 찾기
-for i in range(1, N+1): 
+for i in range(1, N + 1):
     if parentNode[i] == -1:
         root = i
 
+level_min = [N] * (N + 1)
+level_max = [0] * (N + 1)
+level = [0] * (N + 1)
 
-level_min = [N] * (N+1) 
-level_max = [0] * (N+1)
-level = [0] * (N+1)
-
-level[root] = 1 # 루트 레벨 1로 선언
+level[root] = 1  # 루트 레벨 1로 선언
 cur = 0
 
 # 중위 순회
@@ -56,8 +54,8 @@ inorder(root)
 result = 0
 result_idx = 0
 
-for i in range(1, n+1): # 4) 차이 찾기
-    diff = level_max[i]-level_min[i]+1
+for i in range(1, N + 1): # 차이 찾기
+    diff = level_max[i] - level_min[i] + 1
     if result < diff:
         result = diff
         result_idx = i
